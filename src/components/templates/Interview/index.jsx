f35581c9179cd78a4162f79be22c3d8ad009a33a
Rémy Dumas <remy.benjamin.dumas@gmail.com>
2024-03-27 13:46:11 +0100

    Update Eko

--- src/components/templates/Interview/index.jsx
+++ src/components/templates/Interview/index.jsx
@@ -1,30 +1,34 @@
 // Styles
 import { InterviewTemplateStyle } from "./index.style"
+// React
+import { useEffect } from "react";
 // Next
 import { useRouter } from "next/router";
 // Hooks
 import useStore from "@/hooks/useStore";
 // Modules
-import HeaderModule from "@/components/modules/Header"
 import FrameModule from "@/components/modules/Frame"
 // Nodes
-import { EkoVideo } from '@ekolabs/eko-react-sdk';
-import { useRef } from "react";
+// import { EkoVideo } from '@ekolabs/eko-react-sdk';
 
 export default function InterviewTemplate() {
   // Datas
-    const ekoPlayer = useRef();
-    const onPlayerInit = player => {
-      ekoPlayer.current = player;
-    };
-  let playerEventHandlers = {
-    loadeddata: (e) => { console.log(e) },
-    ended: () => onPlayerEnded(),
-  };
+  //   const ekoPlayer = useRef();
+  //   const onPlayerInit = player => {
+  //     ekoPlayer.current = player;
+  //   };
+  // let playerEventHandlers = {
+  //   loadeddata: (e) => { console.log(e) },
+  //   ended: () => onPlayerEnded(),
+  // };
   // Store
   const interviewFinished = useStore((s) => s.interviewFinished)
   // Router
   const router = useRouter();
+  // Effects
+  useEffect(() => {
+    window.addEventListener('message', ekoEvents);
+  }, [])
   // Handlers
   const onPlayerEnded = () => {
     useStore.setState({ 
@@ -48,12 +52,26 @@ export default function InterviewTemplate() {
     }
     return id;
   }
+  const ekoEvents = (event) => {
+    if (!/https?:\/\/(.*?\.)?eko.com/.test(event.origin)) {
+        return;
+    }
+    let msg = event.data;
+    switch (msg.type) {
+        case 'eko.ended':
+          onPlayerEnded()
+            break;
+        default:
+            break;
+    }
+  }
   return (
     <InterviewTemplateStyle>
       <FrameModule>
         <div className="content-interview">
           <div className="content-interview-container">
-            { router.query.slug && <EkoVideo id={ getIdEkoBySlug() } params={ { hidePauseOverlay: true } } onPlayerInit={onPlayerInit} events={playerEventHandlers}/> }
+            { router.query.slug && <iframe src={ `https://video.eko.com/v/${ getIdEkoBySlug() }/embed?autoplay=true&hidePauseOverlay=true&embedapi=1.0&embedid=ekoembed-2&events=canplay%2Cplaying%2Cloadeddata%2Cended%2Csubtitles.visibilitychange%2Csubtitles.substart%2Csubtitles.subend%2Csubtitles.effectivelanguagechange%2Cplugininit` }></iframe> }
+            {/* { router.query.slug && <EkoVideo id={ getIdEkoBySlug() } params={ { hidePauseOverlay: true } } onPlayerInit={onPlayerInit} events={playerEventHandlers}/> } */}
           </div>
         </div>
       </FrameModule>
