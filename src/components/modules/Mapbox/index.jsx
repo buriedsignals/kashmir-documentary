724c838f862bbc447acf1a435ef5877fd5c04e79
Tom Vaillant <tom.vaillant@icloud.com>
2026-09-01 14:33:31 +0200

    chore: load MapTiler key from Vercel environment

--- src/components/modules/Mapbox/index.jsx
+++ src/components/modules/Mapbox/index.jsx
@@ -12,7 +12,7 @@ import "maplibre-gl/dist/maplibre-gl.css";
 // Datas
 import kashmirDatas from "../../../../public/datas/kashmir.geojson";
 
-const MAPTILER_KEY = "__REMOVED_MAPTILER_KEY__";
+const MAPTILER_KEY = process.env.NEXT_PUBLIC_MAPTILER_KEY;
 const BASE_STYLE_URL = `https://api.maptiler.com/maps/backdrop/style.json?key=${MAPTILER_KEY}`;
 const SATELLITE_TILE_URL = `https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=${MAPTILER_KEY}`;
 const SPRITE_URL = `${
